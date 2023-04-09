133d9734184ac2c809674ad3931034e08385d11a
William Babincsak <58189391+TheWillBa@users.noreply.github.com>
2023-04-09 12:06:19 -0400

    now have boxplot - still not matching paper

--- analysis/analyze_output.py
+++ analysis/analyze_output.py
@@ -1,11 +1,14 @@
 from collections import namedtuple
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
 
 Dataline = namedtuple("Dataline", "time robot tolerators attackers")
 
 def parse_votes(token):
 	voters = []
 	for subtoken in token.split(' ')[1:]:
-		if subtoken == '-1' or subtoken == '':
+		if subtoken == '-1' or subtoken == '' or subtoken == "\n":
 			break
 		voters.append(int(subtoken))
 	return voters
@@ -45,6 +48,57 @@ def process_file(file):
 	return lines
 
 
+def time_sus(data: list[Dataline], num_robots):
+	"""
+	Returns the proportion of time the robot is considered faultly for each robot
+
+	Robot is considered faulty if a majority of other robots considers it faulty, and back to normal when no one thinks so
+	"""
+	total_faulty_time = np.zeros(num_robots, dtype=int)
+	currently_faulty = np.zeros(num_robots, dtype=int)
+	declared_faulty_time = np.zeros(num_robots, dtype=int)
+
+	max_time = data[-1].time
+	min_time = data[0].time
+
+	for line in data:
+		is_comm_timestep = int(str(line.time)[-2:]) > 90
+
+		if not is_comm_timestep:
+			continue
+
+		robot_ind = line.robot
+		sus = len(line.attackers) >= num_robots / 2
+
+		# Case 1: Neighbors now think you are faultly
+		if sus and not currently_faulty[robot_ind]:
+			currently_faulty[robot_ind] = 1
+			declared_faulty_time[robot_ind] = line.time
+			if line.robot == 15:
+				print(f"Robot 15 declared fault at {line.time}")
+
+		# Case 2: Neighbors no longer think you are faulty
+		elif not sus and currently_faulty[robot_ind]:
+			currently_faulty[robot_ind] = 0
+			total_faulty_time[robot_ind] = total_faulty_time[robot_ind] + (line.time - declared_faulty_time[robot_ind])
+			declared_faulty_time[robot_ind] = 0
+			if line.robot == 15:
+				print(f"Robot 15 declared safe at {line.time}")
+
+	for robot, faulty in enumerate(list(currently_faulty)):
+		if faulty:
+			total_faulty_time[robot] = total_faulty_time[robot] + (max_time - declared_faulty_time[robot])
+
+
+
+
+	return total_faulty_time / (max_time - min_time)
+
+
+
+
+
+
 
 if __name__ == "__main__":
     
@@ -53,7 +107,20 @@ if __name__ == "__main__":
 	process_dataline("Clock: 453	Id: 0	FV: 55	Consensus_Tolerators:  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1 	Consensus_Attackers:  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1 ")
 	process_dataline('Clock: 1695	Id: 12	FV: 53	Consensus_Tolerators: 0 12 13 14 16 17 18 19 2 3 4 5 7 8 9  -1  -1  -1  -1  -1 	Consensus_Attackers: 1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1  -1 ')
 
-	data = process_file('nohup.txt')
-	print(data)
+	data = process_file('original_data/SWARM_FORAGING/FAULT_ACTUATOR_LWHEEL_SETZERO/nohup_111' if len(sys.argv) < 2 else sys.argv[1])
+	time_faulty = time_sus(data, 20)
+	print(time_faulty)
+
+	tf15 = []
+	for i in range(20):
+		exp = str(i+1)
+		data = process_file('original_data/SWARM_FORAGING/FAULT_ACTUATOR_LWHEEL_SETZERO/nohup_' + exp*3)
+		time_faulty = time_sus(data, 20)
+		tf15.append(time_faulty[15])
+	
+	plt.boxplot(tf15)
+	plt.ylim(0, 1)
+	plt.show()
+
 
         
